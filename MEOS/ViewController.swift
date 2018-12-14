8d60939d236b94bb392e50c8bd6ed423244e5bb1
Виталий <vitaliy@mac-mini-vitalij.local>
2018-12-14 13:03:57 +0500

    Initial Commit

---
+++ MEOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MEOS
+//
+//  Created by Виталий on 14/12/2018.
+//  Copyright © 2018 Vitaliy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
